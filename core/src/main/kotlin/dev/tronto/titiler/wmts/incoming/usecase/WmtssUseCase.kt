b8f070ee410928730271bcf38fd74eedda654739
tronto20 <tronto980@gmail.com>
2024-12-13 16:37:58 +0900

    bug: 이름 버그 수정

--- core/src/main/kotlin/dev/tronto/titiler/wmts/incoming/usecase/WmtssUseCase.kt
+++ core/src/main/kotlin/dev/tronto/titiler/wmts/incoming/usecase/WmtssUseCase.kt
@@ -7,7 +7,7 @@ import dev.tronto.titiler.image.incoming.controller.option.RenderOption
 import dev.tronto.titiler.tile.incoming.controller.option.TileOption
 import dev.tronto.titiler.wmts.incoming.controller.option.WmtsOption
 
-interface WmtsUseCase {
+interface WmtssUseCase {
     suspend fun wmts(
         openOptions: OptionProvider<OpenOption>,
         renderOptions: OptionProvider<RenderOption>,
