b8e1eb069e00b274ba1992b740970f955b0c61f3
tronto20 <tronto980@gmail.com>
2024-12-17 20:03:35 +0900

    fix: logTrace 성능 개선

--- core/src/main/kotlin/dev/tronto/titiler/core/utils/KloggerEx.kt
+++ core/src/main/kotlin/dev/tronto/titiler/core/utils/KloggerEx.kt
@@ -4,13 +4,17 @@ import io.github.oshai.kotlinlogging.KLogger
 import kotlin.time.measureTime
 
 inline fun <T> KLogger.logTrace(name: String, block: () -> T): T {
-    this.trace { "start $name" }
-    var result: Result<T>? = null
-    val time = measureTime {
-        result = kotlin.runCatching {
-            block()
+    if (this.isTraceEnabled()) {
+        this.trace { "start $name" }
+        var result: Result<T>? = null
+        val time = measureTime {
+            result = kotlin.runCatching {
+                block()
+            }
         }
+        this.trace { "end $name - $time" }
+        return result!!.getOrThrow()
+    } else {
+        return block()
     }
-    this.trace { "end $name - $time" }
-    return result!!.getOrThrow()
 }
